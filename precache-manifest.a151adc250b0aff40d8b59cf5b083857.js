57a1f5fcd5e66ab2f6d76132409aaee1d1d378f8
Javier Rivera <jrivera@secureauth.com>
2020-07-30 17:46:58 -0300

    Updates

---
+++ precache-manifest.a151adc250b0aff40d8b59cf5b083857.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d7ca17a7d203cf617d59aaf29dbd0a",
+    "url": "/far-8-githubtest/index.html"
+  },
+  {
+    "revision": "357259f8dce542b29ad3",
+    "url": "/far-8-githubtest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c868c15df24a405aa7d",
+    "url": "/far-8-githubtest/static/js/2.348b1a91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/far-8-githubtest/static/js/2.348b1a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357259f8dce542b29ad3",
+    "url": "/far-8-githubtest/static/js/main.70c6d6d3.chunk.js"
+  },
+  {
+    "revision": "b85705d6c5e358144029",
+    "url": "/far-8-githubtest/static/js/runtime-main.90b01a83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/far-8-githubtest/static/media/logo.5d5d9eef.svg"
+  }
+]);
