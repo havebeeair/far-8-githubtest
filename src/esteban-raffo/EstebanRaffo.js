27ba28de03988d4a35f208530f4052988d4a4e18
Esteban Raffo <e.fraffo@gmail.com>
2020-08-26 15:44:42 -0300

    26/08

---
+++ src/esteban-raffo/EstebanRaffo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const EstebanRaffo = (props) => {
+  return(
+    <>
+      Esteban
+      <Link to="/far-8-githubtest">volver</Link>
+    </>
+  )
+}
+
+export default EstebanRaffo;
