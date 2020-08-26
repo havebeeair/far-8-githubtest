7c68bb6ee2058261fd6c839fe536ae4843f6b10a
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2020-08-25 22:09:00 -0300

    AlfredoZimperz component created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import './App.css';
-import JavierRivera from './javier-rivera/JavierRivera';
-import Dashboard from './Dashboard';
-import { Route } from "react-router-dom";
-
+import React from 'react'
+import './App.css'
+import JavierRivera from './javier-rivera/JavierRivera'
+import AlfredoZimperz from './alfredo-zimperz/AlfredoZimperz'
+import Dashboard from './Dashboard'
+import { Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="grid">
-      <div>
-        <Route exact path="/far-8-githubtest" component={Dashboard} />
-        {/* <Route path="/alfredo-zimperz" component={AlfredoZimperz} />
+	return (
+		<div className="grid">
+			<div>
+				<Route exact path="/far-8-githubtest" component={Dashboard} />
+				<Route path="/alfredo-zimperz" component={AlfredoZimperz} />
+				{/* 
         <Route path="/benjamin-cifre" component={BenjaminCifre} />
         <Route path="/daniel-nicolaevsky" component={DanielNicolaevsky} />
         <Route path="/danitza-bonilla" component={DanitzaBonilla} />
@@ -23,11 +24,10 @@ function App() {
         <Route path="/jorge-fernandez" component={JorgeFernandez} />
         <Route path="/juan-herrera" component={JuanHerrera} />
         <Route path="/nancy-pompei" component={NancyPompei} /> */}
-        <Route path="/javier-rivera" component={JavierRivera} />
-      </div>
-
-    </div>
-  );
+				<Route path="/javier-rivera" component={JavierRivera} />
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/alfredo-zimperz/AlfredoZimperz.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const AlfredoZimperz = () => {
+	return (
+		<>
+			Alfredo
+			<Link to="/far-8-githubtest">volver</Link>
+		</>
+	)
+}
+
+export default AlfredoZimperz
