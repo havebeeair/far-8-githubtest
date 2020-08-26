32d45769ce561f729672e684be7979b4a7e80333
Gustavo Velasquez <gvelasquez@consultoriaglobal.com.ar>
2020-08-26 17:51:58 -0300

    Add link and styles

--- src/gustavo-velasquez/GustavoVelasquez.js
+++ src/gustavo-velasquez/GustavoVelasquez.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "../gustavo-velasquez/GustavoVelasquez.css";
 const GustavoVelasquez = (props) => {
   return(
     <>
       Gustavo
-      <Link to="/far-8-githubtest">volver</Link>
+      <Link className="link" to="/far-8-githubtest">Volver</Link>
     </>
   )
 }
